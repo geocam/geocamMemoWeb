31a013b6e36411bccd9be148d8426baf208f415c
Trey Smith <trey.smith@nasa.gov>
2011-06-16 15:11:49 -0700

    no longer depend on json module

--- geocamMemo/models.py
+++ geocamMemo/models.py
@@ -6,7 +6,7 @@
 
 from django.db import models
 from django.contrib.auth.models import User
-import json
+import geocamUtil.anyjson as json
 import time, datetime
 # from geocamMemo import authentication
 
